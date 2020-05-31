30f157670426e5315a2dd66071860c8086f5951e
blacksmithop <angstycoder101@gmail.com>
2020-05-31 12:27:28 +0530

    added help command

--- cogs/help.py
+++ cogs/help.py
@@ -15,13 +15,14 @@ class Help(commands.Cog):
         cogs = {"🛠 Config": "Configure the bot", "🧾 Log": "Setup logging", "📷 Image": "Image commands"}
         hembed = Embed()
         hembed.title = f"Cogs"
+        hembed.description = f"{self.bot.command_prefix}help [cog] for more information"
         hembed.set_author(name=self.bot.user.display_name,
                           url="https://github.com/blacksmithop/ambu",
                           icon_url=self.bot.user.avatar_url)
         for cog in cogs.keys():
             hembed.add_field(name=cog, value=f"`{cogs[cog]}`", inline=True)
-        hembed.set_footer(text=f"Do {self.bot.command_prefix}help [category] for more",
-                          icon_url=ctx.author.avatar_url)
+        hembed.timestamp = ctx.message.created_at
+        hembed.set_footer(text="•", icon_url=ctx.author.avatar_url)
         await ctx.send(embed=hembed)
 
 
