495af09642a269c7914fad0e69b6c682f6147fac
ambu <60320192+blacksmithop@users.noreply.github.com>
2020-06-18 08:47:54 +0530

    Update minesweeper.py

--- cogs/minesweeper.py
+++ cogs/minesweeper.py
@@ -1,43 +1,9 @@
 import random
-import numpy as np
 from discord.ext import commands
-from discord import Embed
-
-
-class Board:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        number_of_mines = (width - 1) * (height - 1)
-        self.number_of_mines = number_of_mines
-        board = np.array([[0 for i in range(width + 2)] for j in range(height + 2)], dtype=str)
-        self.board = board
-        for i in range(number_of_mines):
-            # generate random co-ords for bombs
-            while True:
-                x_pos, y_pos = random.randint(1, width), random.randint(1, height)
-                if self.board[y_pos, x_pos] != 'b':
-                    self.board[y_pos, x_pos] = 'b'
-                    break
-
-        # iterate over each square of the board
-        for y_pos in range(0, height + 2):
-            for x_pos in range(0, width + 2):
-                # if the square in question is a bomb then skip it
-                if self.board[y_pos, x_pos] == 'b':
-                    continue
-                adj_squares = self.board[max((y_pos - 1), 0):min(height + 2, y_pos + 2),
-                              max((x_pos - 1), 0):min(width + 2, x_pos + 2)]
-                count = 0
-                for square in np.nditer(adj_squares):
-                    if square == 'b':
-                        count += 1
-
-                self.board[y_pos, x_pos] = str(count)
 
 
 class Minesweeper(commands.Cog):
-    """Error handling.
+    """Minesweeper game
     """
 
     def __init__(self, bot):
@@ -45,23 +11,81 @@ class Minesweeper(commands.Cog):
 
     @commands.command(name='ms')
     async def _minesweeper(self, ctx):
-        """
-        Play Minesweeper
-        ?ms
-        """
-        game = Embed(color=0xF10D0D)
-        game.title = "Minesweeper 💣"
-        board = ""
-        test = Board(4, 4)
-        for b in test.board:
-            b = ['||💥||' if x == 'b' else f'||-{x}-||' for x in b]
-            b.append('\n')
-            board += ''.join(b)
-        game.description = board
-        await ctx.send(embed=game)
+        columns = random.randint(4, 13)
+        rows = random.randint(4, 13)
+        bombs = columns * rows - 1
+        bombs = bombs / 2.5
+        bombs = round(random.randint(5, round(bombs)))
+
+        try:
+            columns = int(columns)
+            rows = int(rows)
+            bombs = int(bombs)
+        except ValueError:
+            return
+        if columns > 13 or rows > 13:
+            await ctx.send('The limit for the columns and rows are 13 due to discord limits...')
+            return
+        if columns < 1 or rows < 1 or bombs < 1:
+            await ctx.send('The provided numbers cannot be zero or negative...')
+            return
+        if bombs + 1 > columns * rows:
+            await ctx.send(
+                ':boom:**BOOM**, you have more bombs than spaces on the grid or you attempted to make all of the spaces bombs!')
+            return
+
+        grid = [[0 for num in range(columns)] for num in range(rows)]
+
+        loop_count = 0
+        while loop_count < bombs:
+            x = random.randint(0, columns - 1)
+            y = random.randint(0, rows - 1)
+            if grid[y][x] == 0:
+                grid[y][x] = 'B'
+                loop_count = loop_count + 1
+            if grid[y][x] == 'B':
+                pass
+
+        pos_x = 0
+        pos_y = 0
+        while pos_x * pos_y < columns * rows and pos_y < rows:
+            adj_sum = 0
+            for (adj_y, adj_x) in [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, 1), (1, -1), (-1, -1)]:
+                try:
+                    if grid[adj_y + pos_y][adj_x + pos_x] == 'B' and adj_y + pos_y > -1 and adj_x + pos_x > -1:
+                        adj_sum = adj_sum + 1
+                except Exception:
+                    pass
+
+            if grid[pos_y][pos_x] != 'B':
+                grid[pos_y][pos_x] = adj_sum
+
+            if pos_x == columns - 1:
+                pos_x = 0
+                pos_y = pos_y + 1
+            else:
+                pos_x = pos_x + 1
+
+        string_builder = []
+        for the_rows in grid:
+            string_builder.append(''.join(map(str, the_rows)))
+        string_builder = '\n'.join(string_builder)
+        string_builder = string_builder.replace('0', '||:zero:||')
+        string_builder = string_builder.replace('1', '||:one:||')
+        string_builder = string_builder.replace('2', '||:two:||')
+        string_builder = string_builder.replace('3', '||:three:||')
+        string_builder = string_builder.replace('4', '||:four:||')
+        string_builder = string_builder.replace('5', '||:five:||')
+        string_builder = string_builder.replace('6', '||:six:||')
+        string_builder = string_builder.replace('7', '||:seven:||')
+        string_builder = string_builder.replace('8', '||:eight:||')
+        final = string_builder.replace('B', '||:bomb:||')
+
+        percentage = columns * rows
+        percentage = bombs / percentage
+        percentage = 100 * percentage
+        await ctx.send(content=f'\U0000FEFF\n{final}')
 
 
 def setup(bot):
     bot.add_cog(Minesweeper(bot))
-
-
