63e2b4ae89e358a2447d9b7987e398e67905fa0d
ambu <60320192+blacksmithop@users.noreply.github.com>
2020-06-28 12:39:08 +0530

    Update weather.py

--- cogs/weather.py
+++ cogs/weather.py
@@ -2,12 +2,12 @@ from discord.ext import commands
 from discord import Embed, Color
 from aiohttp import ClientSession
 from json import loads
-from darksky.api import DarkSky, DarkSkyAsync
+from darksky.api import DarkSky
 from datetime import datetime
 from darksky.types import languages, units, weather
+
 KEY = '062938f70cd12c05b1c985bb70761e8f'
 darksky = DarkSky(KEY)
-
 
 
 async def get(session: object, url: object) -> object:
@@ -23,18 +23,29 @@ class Weather(commands.Cog):
         self.bot = bot
 
     @commands.command(name='weather', aliases=['climate'])
-    async def weather(self, ctx):
-        latitude = 11.8745
-        longitude = 75.3704
-        forecast = darksky.get_time_machine_forecast(
-            latitude, longitude,
-            extend=False,  # default `False`
-            lang=languages.ENGLISH,  # default `ENGLISH`
-            values_units=units.AUTO,  # default `auto`
-            exclude=[weather.MINUTELY, weather.ALERTS],  # default `[]`,
-            timezone='UTC',  # default None - will be set by DarkSky API automatically
-            time=datetime.now()
-        )
+    async def weather(self, ctx, *, address):
+        address = '+'.join(address.split())
+        base = f"https://api.opencagedata.com/geocode/v1/json?q={address}&key=54939abc79ce4650a77f3beca24335d2"
+        async with ClientSession() as session:
+            data = await get(session, base)
+        data = loads(data)
+        try:
+            data = data['results'][0]
+            data = data['bounds']['northeast']
+            latitude = data['lat']
+            longitude = data['lng']
+        except:
+            latitude = 11.8745
+            longitude = 75.3704
+        forecast = darksky.get_time_machine_forecast(latitude=latitude, longitude=longitude,
+                                                     extend=False,  # default `False`
+                                                     lang=languages.ENGLISH,  # default `ENGLISH`
+                                                     values_units=units.AUTO,  # default `auto`
+                                                     exclude=[weather.MINUTELY, weather.ALERTS],  # default `[]`,
+                                                     timezone='UTC',
+                                                     # default None - will be set by DarkSky API automatically
+                                                     time=datetime.now()
+                                                     )
         wea = Embed(color=Color.blue())
         wea.set_author(name=forecast.currently.summary)
         wea.add_field(name="Temperature", value=forecast.currently.temperature)
@@ -58,8 +69,6 @@ class Weather(commands.Cog):
         wea.add_field(name="Precipitation", value=forecast.currently.precip_intensity)
         wea.add_field(name="Visibility", value=forecast.currently.visibility)
         return await ctx.send(embed=wea)
-
-
 
 
 def setup(bot):
