605c17fc48f653769bd1a5d5825d9cc8245a66b6
ambu <60320192+blacksmithop@users.noreply.github.com>
2020-07-05 19:18:31 +0530

    Update edit.py

--- cogs/edit.py
+++ cogs/edit.py
@@ -1,8 +1,14 @@
 from discord.ext import commands
 from discord import Embed, Color, Member, File
 from io import BytesIO
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageOps
 from urllib import request
+from aiohttp import ClientSession
+
+
+async def get(session: object, url: object) -> object:
+    async with session.get(url) as response:
+        return await response.read()
 
 
 class Edit(commands.Cog):
@@ -20,10 +26,25 @@ class Edit(commands.Cog):
         file = BytesIO(request.urlopen(url).read())
         img = Image.open(file)
         draw = ImageDraw.Draw(img)
-        font = ImageFont.truetype ("arial.ttf", 18)
 
         w, h = draw.textsize(vic.display_name)
-        draw.text(((227-w)/2, (222-h)/2), vic.display_name, (0, 0, 0), font=font)
+        draw.text(((227-w)/2, (222-h)/2), vic.display_name, (0, 0, 0))
+        arr = BytesIO()
+        img.save(arr, format='PNG')
+        arr.seek(0)
+        file = File(arr, filename="rip.png")
+        await ctx.send(file=file)
+
+    @commands.command()
+    async def invert(self, ctx, vic: Member = None):
+        if vic is None:
+            vic = ctx.author
+        url = str(vic.avatar_url_as(format="png"))
+        async with ClientSession() as session:
+            url = await get(session, url)
+        file = BytesIO(url)
+        img = Image.open(file)
+        img = ImageOps.invert(img.convert('RGB'))
         arr = BytesIO()
         img.save(arr, format='PNG')
         arr.seek(0)
