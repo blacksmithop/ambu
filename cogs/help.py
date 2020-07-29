82a358b828ca3ecaf57a29d2744c79ea0e7b2add
ambu <60320192+blacksmithop@users.noreply.github.com>
2020-07-29 16:27:41 +0530

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -1,6 +1,6 @@
 from discord.ext import commands
 from discord import Embed, Color
-from ambu import db
+import db
 from discord.utils import get
 from aiohttp import ClientSession
 from dotenv import load_dotenv
